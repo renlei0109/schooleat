6279cd83be817b310c7062d73fc65a9d40eb4ec9
renlei0109 <15524878489@163.com>
2014-05-29 00:16:31 +0800

    renlei2

---
+++ src/cn/com/school/eat/code/dao/TestDao.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package cn.com.school.eat.code.dao;
+
+/**
+ * @author renlei 
+ * @E-mail:renlei0109@yeah.net
+ * @version 创建时间：2014-5-29 上午12:16:06 简单说明
+ */
+public class TestDao {
+
+}
